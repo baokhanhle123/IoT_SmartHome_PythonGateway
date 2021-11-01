fc2001508dccb793539c3c46d293579c65048b33
baokhanhle123 <baokhanhle27082001@gmail.com>
2021-11-01 22:15:38 +0700

    Send email

--- main.py
+++ main.py
@@ -7,10 +7,63 @@ import imutils
 
 import pyttsx3
 
-# Want to receive email?
-isSendEmail = True
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
 # Image server link
 imgLink = 'http://smarthomecamera.ddns.net:8888/out.jpg'
+
+# Want to receive email?
+isSendEmail = True
+
+EMAIL_ADDRESS = "bksmartiot@gmail.com"
+EMAIL_PASSWORD = "abcd@12345"
+
+contacts = ['baokhanhle.278@gmail.com', 'test@example.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'Abnormal behavior detected!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = 'baokhanhle.278@gmail.com'
+
+msg.set_content('Suspicious person detected!')
+
+
+def capture_image_email():
+    print("Capture image 1...")
+    frame = imutils.url_to_image(imgLink)
+    cv2.imwrite('img_email1.png', frame)
+
+    print("Capture image 2...")
+    frame = imutils.url_to_image(imgLink)
+    cv2.imwrite('img_email2.png', frame)
+
+    #print("Capture image 3...")
+    #frame = imutils.url_to_image(imgLink)
+    #cv2.imwrite('img_email3.png', frame)
+
+
+def send_email():
+    capture_image_email()
+    files = ['img_email1.png', 'img_email2.png']
+    #files = ['img_email1.png', 'img_email2.png', 'img_email3.png']
+
+    for file in files:
+        with open(file, 'rb') as f:
+            file_data = f.read()
+            file_type = imghdr.what(f.name)
+            file_name = f.name
+
+        msg.add_attachment(file_data, maintype='image', subtype=file_type, filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        print("Logged in...")
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        print("Sending email...")
+        smtp.send_message(msg)
+        print("Email has been sent!")
+
 
 # Setting for audio
 audio = pyttsx3.init()
@@ -20,28 +73,40 @@ audio.setProperty('volume', 0.6)
 # Setting for image capture
 cam = cv2.VideoCapture(0)
 
+
 # Capture image from server
 def capture_image():
     frame = imutils.url_to_image(imgLink)
     cv2.imwrite('img_detect.png', frame)
 
 
-isMask = False
+isSended = False
+
 
 # Face mask detection
 def check_face_mask(index):
+    isMask = False
+    global isSended
+
     if index == 1 or index == 3:
         isMask = False
     elif index == 0:
         isMask = True
+        isSended = False
         audio.setProperty('volume', 0.6)
 
     if not isMask:
+        if isSendEmail and not isSended:
+            send_email()
+            print("Email sended!")
+            isSended = True
+
         audio.say("Wear your face mask")
         if audio.getProperty('volume') <= 0.9:
             audio.setProperty('volume', audio.getProperty('volume') + 0.1)
         elif audio.getProperty('volume') < 1.0:
             audio.setProperty('volume', 1.0)
+
 
 # Face detection
 def face_detection():
@@ -83,8 +148,8 @@ def face_detection():
     print("Ket Qua : ", name[index])
     print("Chinh Xac: ", max_value)
 
+    isSended = False
     check_face_mask(index)
-
 
 while True:
     capture_image()
